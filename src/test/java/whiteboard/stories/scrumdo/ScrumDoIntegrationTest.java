ddc85060feb6763f8c1228cd32979be33bb0e25c
Yani Kereena Atmakuri <yani.kereena@gmail.com>
2012-05-29 07:34:10 +0200

    fix test

--- src/test/java/whiteboard/stories/scrumdo/ScrumDoIntegrationTest.java
+++ src/test/java/whiteboard/stories/scrumdo/ScrumDoIntegrationTest.java
@@ -1,8 +1,11 @@
 package whiteboard.stories.scrumdo;
 
+import com.google.gson.GsonBuilder;
 import org.junit.Before;
 import org.junit.Test;
 import scrumdo.meta.FindResult;
+import scrumdo.meta.GsonBuilderCallback;
+import scrumdo.meta.ResourceURI;
 import scrumdo.meta.Tastypie;
 import scrumdo.model.Story;
 import whiteboard.stories.StoryDetail;
@@ -27,7 +30,12 @@ public class ScrumDoIntegrationTest {
     @Before
     public void setup() {
         // create a "mockup" pie object.
-        Tastypie pie = new Tastypie(null, null, null) {
+        Tastypie pie = new Tastypie(new GsonBuilderCallback() {
+            @Override
+            public void doWithBuilder(GsonBuilder gsonBuilder) {
+                // do nothing
+            }
+        }, null, null) {
             @Override
             public <T> FindResult<T> findAll(Class<T> clazz, Type actualRequestedType) {
                 return (FindResult<T>) stories;
@@ -45,17 +53,20 @@ public class ScrumDoIntegrationTest {
         story.summary = "The story summary";
         story.local_id = 13;
         story.status = 3;
+        story.resource_uri = new ResourceURI("/story/1");
         Story story2 = new Story();
         story2.detail = "The story description 2";
         story2.summary = "The story summary 2";
         story2.local_id = 15;
         story2.status = 1;
+        story2.resource_uri = new ResourceURI("/story/2");
         stories.objects = Arrays.asList(story);
 
         List<StoryDetail> details = testing.getStories();
 
         assertEquals(1, details.size());
         assertEquals("The story description", details.get(0).getStoryDescription());
+        assertEquals("/story/1", details.get(0).getStoryExternalID());
 
     }
 }
